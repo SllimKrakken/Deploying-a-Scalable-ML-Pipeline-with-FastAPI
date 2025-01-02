8f6ffb978334dfe152d75199f769763b3d127ce2
SllimKrakken <millst83@gmail.com>
2025-01-02 14:15:01 -0800

    local test

--- local_api.py
+++ local_api.py
@@ -2,13 +2,12 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+r = requests.get('http://127.0.0.1:8000')
+
+print('GET Status Code:', r.status_code)
+
+print('GET Response:', r.json())
 
 
 
@@ -30,9 +29,8 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post('http://127.0.0.1:8000/data/', json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print('POST Status Code:', r.status_code)
+
+print('POST Result:', r.json())
