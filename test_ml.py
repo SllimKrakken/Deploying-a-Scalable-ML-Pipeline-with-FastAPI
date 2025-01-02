226cb9d1bf2a8bfaad9cd0cdc571e5a6584d4ad0
SllimKrakken <millst83@gmail.com>
2025-01-02 10:43:06 -0800

    test_ml edits

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,35 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+import numpy as np 
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import (
+    inference,
+    train_model,
+)
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
-
-
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+def check_columns():
+    #Check if number of Columns are the same number of columns coming from the Source File
+    path = './data/census.csv'
+    data = pd.read_csv(path)
+    columns = 15
+    columns_read = data.shape[1]
+    assert columns == columns_read
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+def model_inference_test():    
+    #Testing the inference of the current model    
+    X = np.random.rand(20, 5)
+    y = np.random.randint(2, size=30)
+    model_test = train_model(X, y)
+    y_predictions = inference(model_test, X)
+    assert y.shape == y_predictions.shape
+
+
+
+def algorithm_test():
+    #Tests for using the RandomForestClassifier algorithm
+    Xtrain = np.array([[0, 1], [1, 0], [0, 0], [1, 1]])
+    ytrain = np.array([0, 1, 0, 1])  
+    model_algorithm = train_model(Xtrain, ytrain)
+    assert isinstance(model_algorithm, RandomForestClassifier), "RandomForestClassifier is not used for the model"
